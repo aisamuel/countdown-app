0b870789b594c158ecdc5882b167e881e2cba1f9
Samuel <adeniyi.s@turing.com>
2025-02-19 18:14:44 +0100

    updates

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://127.0.0.1:8000/api',
-    wsUrl: 'ws://127.0.0.1:8000/ws' // Ensure WebSocket URL matches Django
+    apiUrl: 'https://institutional-joyan-samuel65-601eb3e3.koyeb.app/api',
+    wsUrl: 'wss://institutional-joyan-samuel65-601eb3e3.koyeb.app/ws'
   };
   
